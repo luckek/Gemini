9e7be83e44075fc6b4e759b14f6273b0a3a0e615
Kyle Lucke <Luckek1354@gmail.com>
2017-10-24 10:17:37 -0600

    Added a few comments to help remind

--- main/src/MgmtAccount.java
+++ main/src/MgmtAccount.java
@@ -5,7 +5,9 @@ public class MgmtAccount {
 	public static String username;
 	public static String password;
 	public static Float accountBalance;
-	public static Array transactions;
+	public static float total;
+	public static Array transactions; // Convert to array of transaction objects
+	public Account[] subAccounts; // Use to hold information of sub-accounts
 	//TODO - uhh...all of this...
 	//need to tie into controller with view in mind, needs to pull control from controller
 	//and inheritance 
@@ -14,4 +16,6 @@ public class MgmtAccount {
 	public static void addTransactions() {}
 	public static void deleteTransactions() {}
 	public static void getBalance() {}
+	// get acctNames
+	// get acctNameBalance(String name)
 }
