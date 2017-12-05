73e7c16ded0fb67325356082874aeaf477ff61f3
Kyle Lucke <Luckek1354@gmail.com>
2017-12-05 09:16:57 -0700

    Added ok button to all balances form

--- main/src/AllBalancesForm.java
+++ main/src/AllBalancesForm.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class AllBalancesForm extends JDialog {
 
@@ -12,6 +14,7 @@ public class AllBalancesForm extends JDialog {
         setPreferredSize(new Dimension(330, 300));
 
         JPanel infoPanel = new JPanel();
+        JPanel bottomPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
 
         // Setting up info table
         JTable acctInfoTable = new JTable(allInfo, columnHeaders);
@@ -19,12 +22,31 @@ public class AllBalancesForm extends JDialog {
 
         JScrollPane infoPane =  new JScrollPane(acctInfoTable);
 
+        JButton okButton = new JButton("Ok");
+        okButton.setPreferredSize(new Dimension(50, 25));
+
         // Populating panels / content pane
-        add(infoPanel);
+        add(infoPanel, BorderLayout.CENTER);
+        add(bottomPanel, BorderLayout.PAGE_END);
 
         infoPanel.add(infoPane);
 
+        bottomPanel.add(okButton);
+
+        okButton.addActionListener(new okAction());
+        getRootPane().setDefaultButton(okButton);
+
         pack();
         setVisible(true);
+    }
+
+    private void close() { this.dispose(); }
+
+    class okAction implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            close();
+        }
     }
 }
